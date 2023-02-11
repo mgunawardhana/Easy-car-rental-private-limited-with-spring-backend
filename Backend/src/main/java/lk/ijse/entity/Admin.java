2b8b0bff6ae1ef8e3ad137f34ec27499fd3cc1ef
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-12 00:24:59 +0530

    content update

---
+++ Backend/src/main/java/lk/ijse/entity/Admin.java
@@ -0,0 +1,36 @@
+package lk.ijse.entity;
+
+
+import lk.ijse.embeded.Name;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+@Entity
+public class Admin {
+
+    @Id
+    @Column(name = "admin_id")
+    private String adminId;
+    @Column(name = "nic_no")
+    private String adminNic;
+    @Embedded
+    @Column(name = "full_name")
+    private Name adminName;
+    @Column(name = "address")
+    private String adminAddress;
+    @Column(name = "email")
+    private String adminEmail;
+    @Column(name = "contact_no")
+    private String adminContact;
+    @OneToOne(cascade = CascadeType.ALL)
+    private User user;
+}
