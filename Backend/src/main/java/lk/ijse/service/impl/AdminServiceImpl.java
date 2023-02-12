5bcee1cd44c5a33bb4e3f785617d06b536bb75da
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-12 15:00:19 +0530

    content update

--- Backend/src/main/java/lk/ijse/service/impl/AdminServiceImpl.java
+++ Backend/src/main/java/lk/ijse/service/impl/AdminServiceImpl.java
@@ -35,7 +35,11 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public void deleteAdmin(String id) {
-
+        if (adminRepo.existsById(id)) {
+            adminRepo.deleteById(id);
+        } else {
+            throw new RuntimeException("No such a admin like this !");
+        }
     }
 
     @Override
