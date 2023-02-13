2e29c60b28367ac69b150c442823ebd0fad49f6c
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-13 09:12:13 +0530

    introducing driverService

--- Backend/src/main/java/lk/ijse/service/DriverService.java
+++ Backend/src/main/java/lk/ijse/service/DriverService.java
@@ -1,4 +1,13 @@
 package lk.ijse.service;
 
+
+import lk.ijse.dto.DriverDTO;
+
+import java.util.List;
+
 public interface DriverService {
+    void saveDriver(DriverDTO driverDTO);
+    void deleteDriver(String id);
+    void updateDriver(DriverDTO driverDTO);
+    List<DriverDTO> getAllDriver();
 }
