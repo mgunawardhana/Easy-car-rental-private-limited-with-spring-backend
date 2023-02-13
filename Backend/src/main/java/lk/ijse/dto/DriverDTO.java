fe48507b3716efc169894d3ade8689a9e5479fa3
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-13 09:08:12 +0530

    driverDTO setiped

--- Backend/src/main/java/lk/ijse/dto/DriverDTO.java
+++ Backend/src/main/java/lk/ijse/dto/DriverDTO.java
@@ -1,4 +1,24 @@
 package lk.ijse.dto;
 
+import lk.ijse.embeded.Name;
+import lk.ijse.enums.AvailabilityType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
 public class DriverDTO {
+    private String id;
+    private String nic;
+    private Name name;
+    private String address;
+    private String drivingLicenseNo;
+    private String email;
+    private String contactNo;
+    private UserDTO user;
+    private AvailabilityType driverAvailability;
 }
---
+++ Backend/src/main/java/lk/ijse/enums/AvailabilityType.java
@@ -0,0 +1,9 @@
+package lk.ijse.enums;
+
+import lombok.ToString;
+@ToString
+public enum AvailabilityType {
+
+    AVAILABLE, NOT_AVAILABLE
+
+}
