6de9b3aff9dcc38c01749983718b611547c882ef
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-13 09:13:28 +0530

    introducing driverServiceImpl

--- Backend/src/main/java/lk/ijse/service/impl/DriverServiceImpl.java
+++ Backend/src/main/java/lk/ijse/service/impl/DriverServiceImpl.java
@@ -1,4 +1,28 @@
 package lk.ijse.service.impl;
 
-public class DriverServiceImpl {
+import lk.ijse.dto.DriverDTO;
+import lk.ijse.service.DriverService;
+
+import java.util.List;
+
+public class DriverServiceImpl implements DriverService {
+    @Override
+    public void saveDriver(DriverDTO driverDTO) {
+
+    }
+
+    @Override
+    public void deleteDriver(String id) {
+
+    }
+
+    @Override
+    public void updateDriver(DriverDTO driverDTO) {
+
+    }
+
+    @Override
+    public List<DriverDTO> getAllDriver() {
+        return null;
+    }
 }
