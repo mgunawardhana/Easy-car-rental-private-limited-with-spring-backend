1a817e9fafbe53146c0166ebaa5f56e954634ef3
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-13 22:29:08 +0530

    content update

---
+++ Backend/src/main/java/lk/ijse/dto/VehicleDTO.java
@@ -0,0 +1,36 @@
+package lk.ijse.dto;
+
+
+import lk.ijse.embeded.Mileage;
+import lk.ijse.embeded.PriceRate;
+import lk.ijse.enums.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@ToString
+public class VehicleDTO {
+
+    private String vehicleId;
+    private String registrationNo;
+    private String vehicleBrand;
+    private VehicleType vehicleType;
+    private FuelType fuelType;
+    private int numberOfPassenger;
+    private String vehicleColour;
+
+
+    private TransmissionType transmissionType;
+    private double refundableDamagedFee;
+    private int vehicleMileage;
+    private PriceRate vehiclePriceRate;
+    private Mileage freeMileage;
+    private int lastServiceMileage;
+    private double extraKmPer;
+    private AvailabilityType vehicleAvailability;
+    private RequestType needMaintenance;
+}
---
+++ Backend/src/main/java/lk/ijse/service/VehicleService.java
@@ -0,0 +1,13 @@
+package lk.ijse.service;
+
+import lk.ijse.dto.DriverDTO;
+import lk.ijse.dto.VehicleDTO;
+
+import java.util.List;
+
+public interface VehicleService {
+    void saveVehicle(VehicleDTO driverDTO);
+    void deleteVehicle(String id);
+    void updateVehicle(VehicleDTO driverDTO);
+    List<VehicleDTO> getAllVehicle();
+}
