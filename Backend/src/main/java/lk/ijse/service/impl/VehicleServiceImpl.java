b6547058f3810d59e67a9ccbda903b5f7c976318
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-02-13 22:33:56 +0530

    content update

--- Backend/src/main/java/lk/ijse/service/impl/VehicleServiceImpl.java
+++ Backend/src/main/java/lk/ijse/service/impl/VehicleServiceImpl.java
@@ -1,4 +1,32 @@
 package lk.ijse.service.impl;
 
-public class VehicleServiceImpl {
+import lk.ijse.dto.VehicleDTO;
+import lk.ijse.service.VehicleService;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+
+@Service
+@Transactional
+public class VehicleServiceImpl implements VehicleService {
+    @Override
+    public void saveVehicle(VehicleDTO driverDTO) {
+
+    }
+
+    @Override
+    public void deleteVehicle(String id) {
+
+    }
+
+    @Override
+    public void updateVehicle(VehicleDTO driverDTO) {
+
+    }
+
+    @Override
+    public List<VehicleDTO> getAllVehicle() {
+        return null;
+    }
 }
