1f3ff12d88aadf80efa21507864db5b5232f4405
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-05 15:02:05 +0530

    revenue genarator for current date and time

--- Frontend/controllers/IncomeReports.js
+++ Frontend/controllers/IncomeReports.js
@@ -2,6 +2,8 @@ let baseURL = "http://localhost:8080/Backend_war/";
 
 
 getAllByDailyRevenues();
+gettingRevenueByYear();
+
 
 function getAllByDailyRevenues() {
     $.ajax({
@@ -12,6 +14,27 @@ function getAllByDailyRevenues() {
 
                 // let row = "<tr>" + "<td>" +c+ "</td>" + "<td>" + c + "</td>"+"</tr>";
                     $("#incomeReportsDailyTable").append(c+"<br>");
+            }
+
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
+function gettingRevenueByYear() {
+
+
+
+    $("#todayDate").val(new Date().getFullYear()+" / "+new Date().getMonth()+" / "+new Date().getDate()+"   "
+        +new Date().getHours()+" : "+new Date().getMinutes()+" : "+new Date().getSeconds());
+
+    $.ajax({
+        url: baseURL + "income/by_year", dataType: "json", success: function (res) {
+            console.log(res.data);
+            for (let c of res.data) {
+                let id = c;
+                $("#revenueByYear").val("Rs "+id+".00");
             }
 
         }, error: function (error) {
