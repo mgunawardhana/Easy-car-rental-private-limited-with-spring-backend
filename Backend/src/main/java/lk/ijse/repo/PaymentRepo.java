34a6f7f7cd3643650ca7edba349e8d1f0d33f823
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-06 09:43:42 +0530

    content update

--- Backend/src/main/java/lk/ijse/repo/PaymentRepo.java
+++ Backend/src/main/java/lk/ijse/repo/PaymentRepo.java
@@ -2,6 +2,12 @@ package lk.ijse.repo;
 
 import lk.ijse.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PaymentRepo extends JpaRepository<Payment, String> {
+
+    @Query(value = "SELECT * FROM payment;", nativeQuery = true)
+    List getAllPaymentDetails();
 }
--- Frontend/controllers/PaymentFormController.js
+++ Frontend/controllers/PaymentFormController.js
@@ -38,33 +38,6 @@ $("#save").on('click', function () {
 });
 
 
-function getAllPaymentDetails() {
-    $("#paymentTable").empty();
-    $.ajax({
-        url: baseURL + "/payment/get_all_payment_details", dataType: "json", success: function (res) {
-            for (let c of res.data) {
-                console.log(c)
-
-                let paymentID = c.paymentId;
-                let paymentDate = c.paymentDate;
-                let invoiceNo = c.invoiceNo;
-                let amount = c.amount;
-                let paymentType = c.paymentDate;
-                let booking = c.booking;
-
-
-                let row = "<tr>" + "<td>" + paymentID + "</td>" + "<td>" + invoiceNo + "</td>" + "<td>" + paymentDate + "</td>" + "<td>" + booking + "</td>" + "<td>" + paymentType + "</td>" + "<td>" + amount + "</td>" + "</tr>";
-                $("#paymentTable").append(row);
-            }
-
-        }, error: function (error) {
-            let message = JSON.parse(error.responseText).message;
-            alert(message);
-        }
-    });
-}
-
-
 loadAllCustomersToCombo();
 
 function loadAllCustomersToCombo() {
@@ -96,3 +69,31 @@ $('#bookingId').on('click', function () {
         }
     });
 });
+
+
+
+function getAllPaymentDetails() {
+    $("#paymentTable").empty();
+    $.ajax({
+        url: baseURL + "/payment/get_all_payment_details", dataType: "json", success: function (res) {
+            for (let c of res.data) {
+                console.log(c)
+
+                let paymentID = c.paymentId;
+                let paymentDate = c.paymentDate;
+                let invoiceNo = c.invoiceNo;
+                let amount = c.amount;
+                let paymentType = c.paymentDate;
+                let booking = c.booking;
+
+
+                let row = "<tr>" + "<td>" + paymentID + "</td>" + "<td>" + invoiceNo + "</td>" + "<td>" + paymentDate + "</td>" + "<td>" + booking + "</td>" + "<td>" + paymentType + "</td>" + "<td>" + amount + "</td>" + "</tr>";
+                $("#paymentTable").append(row);
+            }
+
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
+}
