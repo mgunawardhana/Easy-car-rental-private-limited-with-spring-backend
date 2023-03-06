349624b2611f49807b3c74a224ada72928ef0bc2
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-06 12:58:16 +0530

    content update

--- Frontend/controllers/IncomeReports.js
+++ Frontend/controllers/IncomeReports.js
@@ -7,6 +7,7 @@ gettingRevenueByYear();
 
 let val1;
 let val2;
+
 function getAllByDailyRevenues() {
     $.ajax({
         url: baseURL + "income/by_daily", dataType: "json", success: function (res) {
@@ -21,28 +22,41 @@ function getAllByDailyRevenues() {
         }
     });
 }
+
 let month;
+
 function getAllByMonthlyRevenues() {
     $.ajax({
         url: baseURL + "income/by_month", dataType: "json", success: function (res) {
             for (let c of res.data) {
-                if (c[0]===1){
-                    month="January";
-                }else if(c[0]===2){
-                    month ="February";
-                }else if(c[0]===3){
-                    month="March";
+                if (c[0] === 1) {
+                    month = "January";
+                } else if (c[0] === 2) {
+                    month = "February";
+                } else if (c[0] === 3) {
+                    month = "March";
+                } else if (c[0] === 4) {
+                    month = "April";
+                } else if (c[0] === 5) {
+                    month = "May";
+                } else if (c[0] === 6) {
+                    month = "June";
+                } else if (c[0] === 7) {
+                    month = "July";
+                } else if (c[0] === 8) {
+                    month = "August";
+                } else if (c[0] === 9) {
+                    month = "September";
+                } else if (c[0] === 10) {
+                    month = "October";
+                } else if (c[0] === 11) {
+                    month = "November";
+                } else if (c[0] === 12) {
+                    month = "December";
                 }
-
-
-
-
 
                 let row = "<tr>" + "<td>" + month + "</td>" + "<td>" + (c[1]) + "</td>" + "</tr>";
                 $("#incomeReportsMonthlyTable").append(row);
-
-
-
 
 
             }
@@ -55,20 +69,16 @@ function getAllByMonthlyRevenues() {
 }
 
 
-
-
-
-
 function gettingRevenueByYear() {
-    $("#todayDate").val(new Date().getFullYear()+" / "+new Date().getMonth()+" / "+new Date().getDate()+"   "
-        +new Date().getHours()+" : "+new Date().getMinutes()+" : "+new Date().getSeconds());
+    $("#todayDate").val(new Date().getFullYear() + " / " + new Date().getMonth() + " / " + new Date().getDate() + "   "
+        + new Date().getHours() + " : " + new Date().getMinutes() + " : " + new Date().getSeconds());
 
     $.ajax({
         url: baseURL + "income/by_year", dataType: "json", success: function (res) {
             console.log(res.data);
             for (let c of res.data) {
                 let id = c;
-                $("#revenueByYear").val("Rs "+id+".00");
+                $("#revenueByYear").val("Rs " + id + ".00");
             }
 
         }, error: function (error) {
