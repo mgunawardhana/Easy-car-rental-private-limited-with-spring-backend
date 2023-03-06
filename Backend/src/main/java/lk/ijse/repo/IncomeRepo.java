df924485c4cabf5fe4f54b9778d771d807a9e609
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-06 12:30:24 +0530

    content update

--- Backend/src/main/java/lk/ijse/repo/IncomeRepo.java
+++ Backend/src/main/java/lk/ijse/repo/IncomeRepo.java
@@ -12,6 +12,12 @@ public interface IncomeRepo extends JpaRepository<Payment, String> {
     @Query(value = "SELECT SUM(amount),date FROM payment GROUP BY date", nativeQuery = true)
     List getRevenueByDate();
 
+    @Query(value = "SELECT MONTH(date) AS month, SUM(amount) AS monthly_amount FROM payment GROUP BY MONTH(date)", nativeQuery = true)
+    List getRevenueByMonth();
+
     @Query(value = "SELECT SUM(amount)FROM payment;", nativeQuery = true)
     List getRevenueByYear();
+
+
+
 }
