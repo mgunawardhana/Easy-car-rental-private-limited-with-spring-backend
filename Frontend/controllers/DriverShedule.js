f12f656ccd00fe213e8c60eec8a71f73d5f27550
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-03-07 14:12:56 +0530

    adding login form and another driver schedule viewer

--- Frontend/controllers/DriverShedule.js
+++ Frontend/controllers/DriverShedule.js
@@ -1,8 +1,23 @@
 let baseURL = "http://localhost:8080/Backend_war/driver/";
 
 getAllDrivers();
+
+let bookingId;
+let driverId;
+
 function getAllDrivers() {
     $("#driverTableBody").empty();
+    $.ajax({
+        url: baseURL + "get_driver_schedule", success: function (res) {
+            for (let c of res.data) {
+                 bookingId= c[0];
+                 driverId= c[1];
+            }
+        }, error: function (error) {
+            let message = JSON.parse(error.responseText).message;
+            alert(message);
+        }
+    });
     $.ajax({
         url: baseURL + "get_all", success: function (res) {
             for (let c of res.data) {
@@ -14,17 +29,20 @@ function getAllDrivers() {
                 let contactNo = c.contactNo;
                 let nic = c.nic;
 
+                if (id === driverId){
 
-                let row = "<tr>" +
-                    "<td>" + id + "</td>"
-                    + "<td>" + firstname + "</td>"
-                    + "<td>" + lastname + "</td>"
-                    + "<td>" + drivingLicenseNo + "</td>"
-                    + "<td>" + contactNo + "</td>"
-                    + "<td>" + nic + "</td>"
-                    + "</tr>";
+                    let row = "<tr>" +
+                        "<td>" + id + "</td>"
+                        + "<td>" + bookingId + "</td>"
+                        + "<td>" + firstname + "</td>"
+                        + "<td>" + lastname + "</td>"
+                        + "<td>" + drivingLicenseNo + "</td>"
+                        + "<td>" + contactNo + "</td>"
+                        + "<td>" + nic + "</td>"
+                        + "</tr>";
 
-                $("#driverTableBody").append(row);
+                    $("#driverTableBody").append(row);
+                }
             }
         }, error: function (error) {
             let message = JSON.parse(error.responseText).message;
--- Frontend/controllers/loginFormController.js
+++ Frontend/controllers/loginFormController.js
@@ -2,18 +2,38 @@ let baseURL = "http://localhost:8080/Backend_war/";
 
 
 $("#login").on('click', function () {
-    alert("Your entering as a ADMIN");
+
     $.ajax({
         url: baseURL + "login_form/?userName=" + $("#name").val() + "&password=" + $("#password").val(),
         method: "GET",
         dataType: "json",
+
         success: function (resp) {
-            if ("Ok" === resp.state) {
-                window.location.href = "DashBoardForm.html";
-            } else {
-                alert(resp.state);
+
+            if ('ADMIN' === $("#role").val()) {
+
+                alert("Your entering as a ADMIN");
+
+                if ("Ok" === resp.state) {
+                    window.location.href = "DashBoardForm.html";
+                } else {
+                    alert(resp.state);
+                }
+
+            } else if ('DRIVER' === $("#role").val()) {
+
+                alert("Your entering as a DRIVER");
+
+                alert($("#role").val());
+                if ("Ok" === resp.state) {
+                    window.location.href = "DriverShedule.html";
+                } else {
+                    alert(resp.state);
+                }
+
             }
         },
+
         error: function (error) {
             alert(JSON.parse(error.responseText).message);
         }
